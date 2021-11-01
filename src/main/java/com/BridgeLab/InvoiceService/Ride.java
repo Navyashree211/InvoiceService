fbb4b6dc940ba411964c616b75cab5b8cfd44666
unknown <navyas211@gmail.com>
2021-11-01 12:28:34 +0530

    [NAVYA]ADDED: Invoice Generator should now take in multiple rides, and calculate the aggregate total for all

---
+++ src/main/java/com/BridgeLab/InvoiceService/Ride.java
@@ -0,0 +1,12 @@
+package com.BridgeLab.InvoiceService;
+
+public class Ride {   
+	 public int distance;
+	    public int time;
+
+	    public Ride(int distance, int time) {
+	        this.distance = distance;
+	        this.time = time;
+	    }
+
+}
